c6cd684dff3d0104c75f60b1431d87317f689033
rodavok <rodavok@gmail.com>
2025-04-21 16:06:19 -0400

    fixed size of tohjo giovanni room

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -146,7 +146,7 @@ ENDM
 	map_const DRAGONS_DEN_B1F,                             20, 18 ; 81
 	map_const DRAGON_SHRINE,                                5,  5 ; 82
 	map_const TOHJO_FALLS,                                 15,  9 ; 83
-	map_const TOHJO_FALLS_GIOVANNI_ROOM,				    4,  4 ; 84	
+	map_const TOHJO_FALLS_GIOVANNI_ROOM,				    5,  5 ; 84	
 	map_const DIGLETTS_CAVE,                               10, 18 ; 85
 	map_const MOUNT_MOON,                                  15,  9 ; 86
 	map_const UNDERGROUND_PATH,                             3, 14 ; 87
